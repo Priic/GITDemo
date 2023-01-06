c5a70a12c843dc3cb1f56e3ee9bb1685c4fac7fd
Priic <prichaseofc@gmail.com>
2023-01-06 13:44:47 -0600

    My first commit in Github demo

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+public class AppiumTest {
+	
+	public class NativeAppAndroid {
+		
+		@Test 
+		public void MobileLogin()
+		{
+			System.out.println("NativeAPP Android testcases");
+		}
+		}
+		
+		@Test 
+		public void IOSApps()
+		{
+			System.out.println("Appium IOS apps testcases");
+		}
+
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void SeleniumBrowserAutomation()
+	
+	{
+		System.out.println("Selenium browser Automation testcases");
+	}
+	
+	@Test
+public void SeleniumDisplayResult()
+	
+	{
+		System.out.println("Selenium Dispaly result testcases");
+	}
+
+
+}
